f0f91d17e3c0698a15fb209f3ede9c5543e6064f
gyuhan <leegyuhan21@gmail.com>
2024-02-05 01:16:33 +0900

    maiadb 연동

--- src/main/java/com/todayonuel/myhome/controller/BoardController.java
+++ src/main/java/com/todayonuel/myhome/controller/BoardController.java
@@ -1,15 +1,27 @@
 package com.todayonuel.myhome.controller;
 
+
+import com.todayonuel.myhome.model.Board;
+import com.todayonuel.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import static org.antlr.v4.runtime.tree.xpath.XPath.findAll;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-
+    @Autowired
+    private BoardRepository boardRepository;
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
         return  "board/list";
     }
 }
---
+++ src/main/java/com/todayonuel/myhome/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.todayonuel.myhome.repository;
+
+import com.todayonuel.myhome.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+}
